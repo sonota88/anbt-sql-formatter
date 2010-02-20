06e618c5f07a5f586e129c7200b381822031e2e6
sonota <yosiot8753@gmail.com>
2010-02-20 19:54:04 +0900

    added test/helper.rb

---
+++ test/helper.rb
@@ -0,0 +1,17 @@
+require "test/unit"
+require "pp"
+
+$LOAD_PATH.unshift File.join(File.dirname(__FILE__), "..", "lib")
+
+class Helper
+  def Helper.format_tokens(list)
+    list.map{|token|
+      "<#{token._type}>#{token.string}</>"
+    }.join("\n")
+  end
+end
+
+
+def assert_equals(a,b,c)
+  assert_equal(b,c,a)
+end
